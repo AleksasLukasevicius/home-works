5dd5bfb189caff6ca4480c07fb0ca74875baf208
AleksasLukasevicius <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2022-09-07 16:19:33 +0300

    task two exercises

---
+++ JS/IF-ELSEIF-ELSE.js
@@ -0,0 +1,39 @@
+// const legalAge = 20;
+// const clientAge = prompt("Koks jūsų amžius?");
+
+// if (clientAge >= legalAge) {
+//     alert(`Pasiekęs ${legalAge}`);
+// } else {
+//     alert(`Nepasiekęs ${legalAge}`);
+// }
+
+// alert(age)
+
+// const str = prompt("Koks jūsų vardas?");
+
+// if (str.length > 6) {
+//     alert("Ilgas vardas")
+// }
+// alert(`${str} is ${str.length} name units long`)
+
+
+// const x = prompt("Koks jūsų amžius?");
+
+// if (x > 100 || x < 0) {
+//   alert("Invalid age")
+// }
+// else if (x >= 1 && x <= 18) {
+//   alert("Child")
+// }
+// else if (x >= 19 && x <= 99) {
+//   alert("Adult")
+// }
+
+const car = prompt("Koks jūsų auto?");
+
+if (car === "VW" || car === "Audi" || car === "Bentley" || car === "Bugatti" || car === "Lamborghini" || car === "Porsche") {
+    alert("priklauso VW group")
+}
+else if (car === "BMW" || car === "Mini" || car === "Rolls-Royce") {
+    alert("priklauso BMV")
+}

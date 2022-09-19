ce3dc5a7528a6e4f3f7c0609b3596df25058fe50
AleksasLukasevicius <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2022-09-19 10:21:55 +0300

    Lesson 12 task number metods

--- JS/Lesson12/metods.js
+++ JS/Lesson12/metods.js
@@ -1,3 +1,22 @@
-const isLegalAge = true;
-console.info(isLegalAge);
-console.info(isLegalAge.toString);
+// const isLegalAge = true;
+// console.info(isLegalAge);
+// console.info(isLegalAge.toString);
+
+// const milkPrice = 3;
+// alert(Number.isInteger(milkPrice) ? "nereikia centu" : "reikes centu")
+// alert(milkPrice.toFixed(2))
+
+
+
+document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const price = Number(e.target.elements.price.value);
+    const litrs = Number(e.target.elements.litrs.value);
+
+    const totalPrice = price * litrs;
+
+    const priceDisplay = document.createElement("h1");
+    priceDisplay.textContent = totalPrice.toFixed(2) + " €";
+    document.body.append(priceDisplay);
+});

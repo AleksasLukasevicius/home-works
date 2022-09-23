448882ae9e9389fe304641e13196d7db70d956d9
AleksasLukasevicius <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2022-09-23 12:27:58 +0300

    Lesson 14 array metods

--- JS/Lesson14/array-metods.js
+++ JS/Lesson14/array-metods.js
@@ -25,6 +25,26 @@ const people = [
     }
 ];
 
-console.log(people.filter(x => x.age >= 18))
-console.log(people.filter(x => x.age >= 18).map(user => user.name))
+console.log(people.filter(x => x.age >= 18));
 
+console.log(people.filter(x => x.age >= 18).map(user => user.name));
+
+console.log(people.filter(x => x.age >= 18).map(user => user.name).sort());
+
+
+
+const cars = [
+    { carName: "Honda", year: 2002 },
+    { carName: "Saab", year: 1998 },
+    { carName: "Lada", year: 1976 },
+    { carName: "Mitsubishi", year: 1992 },
+    { carName: "MitsuVWbishi", year: 2006 },
+    { carName: "Tesla", year: 2020 }
+]
+
+function showCarsByYear(carsByYear) {
+    console.log(carsByYear.sort((a, b) => a.year - b.year))
+    return { oldest: carsByYear[0].carName, youngest: carsByYear[carsByYear.length - 1].carName }
+}
+
+console.log(showCarsByYear(cars))

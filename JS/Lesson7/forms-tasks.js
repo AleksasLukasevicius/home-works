5cc6cc1e6065e33d1d054265c3c7e0814a1da1f8
ALEKSAS LUKAŠEVIČIUS <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2022-09-26 23:20:37 +0300

    Lesson 7 1 forms

--- JS/Lesson7/forms-tasks.js
+++ JS/Lesson7/forms-tasks.js
@@ -1,13 +1,18 @@
 const form = document.querySelector("form");
 
-form.addEventListener("submit", (evebt) => {
+form.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const years = document.getElementById(age).value;
+    const years = document.getElementById('age').value;
+    console.info(years);
 
-    const bonus = 50;
-    if (years >= 10) { bonus += 50; }
-    if (years >= 20) { bonus += 100; }
+    let bonus = 50;
+    if (years >= 10) {
+        bonus += 50;
+    }
+    if (years >= 20) {
+        bonus += 100;
+    }
 
-    document.getElementById("h2").innerText = `Jūsų premija: ${bonus}`;
+    document.getElementsByClassName('h2').innerText = `Jūsų premija: ${bonus}`;
 })

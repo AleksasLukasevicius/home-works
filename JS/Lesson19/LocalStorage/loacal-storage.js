099173498c98fe845f10f644dea90d238a906bdc
ALEKSAS LUKAŠEVIČIUS <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2022-10-12 23:56:45 +0300

    Lesson 19 Local Storage fix

--- JS/Lesson19/LocalStorage/loacal-storage.js
+++ JS/Lesson19/LocalStorage/loacal-storage.js
@@ -1,28 +1,27 @@
 const userProfileInfo = {
-    userFirstName: "",
-    userSurName: "",
-    userPhoto: "https://saldireklama.lt/upload/medialibrary/416/416ba733792970c865d5efd6c95d2a81.jpg",
-    userDescription: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque excepturi quos enim dolor, dolore aliquam inventore! Quasi voluptatem, nemo rem, ea nulla necessitatibus odio officia debitis laborum consequuntur totam iste?"
+    userFirstName: 'Aleksas',
+    userLastName: 'Lukaševičius',
+    userPicture: 'https://saldireklama.lt/upload/medialibrary/416/416ba733792970c865d5efd6c95d2a81.jpg',
+    userDescription: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque excepturi quos enim dolor, dolore aliquam inventore! Quasi voluptatem, nemo rem, ea nulla necessitatibus odio officia debitis laborum consequuntur totam iste?',
 }
+localStorage.setItem('profile', JSON.stringify(userProfileInfo));
 
-localeStorage.setItem("profile", JSON.stringify(userProfileInfo));
+const renderUserProfileCard = (profile) => {
+    const userProfilePicture = document.createElement('img');
+    userProfilePicture.width = '200';
+    userProfilePicture.src = profile.userPicture;
+    userProfilePicture.alt = `${profile.userFirstName} ${userProfileInfo.userLastName} profile picture`;
 
-const creatUserProfileCard = (userProfileInfo) => {
-    const userImage = document.createElement("user-photo");
-    userImage.width = "200";
-    userImage.src = userProfileInfo.userPhoto;
-    userImage.alt = `${userProfileInfo.userFirstName} ${userProfileInfo.userSurName} profile picture`;
+    const userFullName = document.createElement('h4');
+    userFullName.innerText = `${profile.userFirstName} ${profile.userLastName}`;
 
-    const userFullName = document.createElement("h4");
-    userFullName.innerText = `${userFirstName} ${userSurName}`;
+    const userDescription = document.createElement('h5');
+    userDescription.innerText = profile.userDescription;
 
-    const userDescription = document.createElement("h5");
-    userDescription.innerText = userProfileInfo.userDescription;
+    const card = document.createElement('div');
+    card.append(userProfilePicture, userFullName, userDescription);
+    document.body.append(card);
+};
 
-    const userCard = document.createElement("div");
-    userCard.append(userImage, userFullName, userDescription);
-    document.body.append(userCard);
-}
-
-const profileInfoFromLocalStorage = JSON.parse(localStorage.getItem("profile"));
-creatUserProfileCard(profileInfoFromLocalStorage);
+const profileInfoFromLocalStorage = JSON.parse(localStorage.getItem('profile'));
+renderUserProfileCard(profileInfoFromLocalStorage);

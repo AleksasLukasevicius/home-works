aabf2aa529a056548dd066c4c16c63dd84bd65d4
AleksasLukasevicius <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2022-10-24 16:51:49 +0300

    Lesson 10 fix

--- JS/Lesson10/objects.js
+++ JS/Lesson10/objects.js
@@ -1,35 +1,44 @@
-// const cars = {
+// 1 pratymas
+
+// const car = {
 //     doors: 4,
 //     color: "red",
 //     brand: "Honda"
 // }
-// console.info(cars.doors);
+
+// console.info(car.doors);
 
 
-document.forms[0].addEventListener("submit", e => {
-    e.preventDefault();
+// 2 pratymas
 
-    // const userName = e.target.elements.name.value;
-    const age = Number(e.target.elements.age.value);
-    const isLegalAge = age >= 18;
-
-    const person = {
-        userName: e.target.elements.name.value,
-        isLegal: isLegalAge,
-    }
-
-    console.info(person);
-})
 // document.forms[0].addEventListener("submit", e => {
 //     e.preventDefault();
 
-//     // const userName = e.target[0].value;
-//     // const userSurName = e.target[1].value;
+//     const userName = e.target[0].value;
+//     const userSurName = e.target[1].value;
 
 //     const person = {
-//         userName: e.target[0].value,
-//         userSurName: e.target[1].value,
+//         userName,
+//         userSurName
 //     }
 
 //     console.info(person);
 // })
+
+// 3 pratymas
+
+document.forms[0].addEventListener("submit", e => {
+    e.preventDefault();
+
+    const userName = e.target.elements.name.value;
+    const age = Number(e.target.elements.age.value);
+
+    const isLegalAge = age >= 18;
+
+    const person = {
+        userName,
+        isLegal: isLegalAge,
+    }
+
+    console.info({ person });
+});

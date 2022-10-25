e5055287b8eec63818d07b025648fcf66215fc82
ALEKSAS LUKAŠEVIČIUS <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2022-10-26 00:57:51 +0300

    Lesson 22 MOB fix

--- JS/Lesson22 full/modules/fetch.js
+++ JS/Lesson22 full/modules/fetch.js
@@ -3,6 +3,8 @@ const getRobots = async () => {
         const response = await fetch("https://magnetic-melon-yam.glitch.me");
         const robots = await response.json();
 
+        console.info({ robots })
+
         return robots;
     } catch (error) {
         console.error(error)

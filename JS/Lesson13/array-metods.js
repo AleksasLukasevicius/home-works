a978f09a577e60c3bb8744bd3ecf146c494acced
AleksasLukasevicius <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2022-10-26 16:44:55 +0300

    Lesson 13 fix

--- JS/Lesson13/array-metods.js
+++ JS/Lesson13/array-metods.js
@@ -1,29 +1,29 @@
 // 2 pratymas
 const cars = ["BMW", "VW", "Audi"];
-cars.forEach((v, i) => console.info(i + ": " + v));
+cars.forEach((v, i) => console.info(`${i}: ${v}`));
 
 // 3 pratymas
 const friendsNames = ["peTras", "Jonas", "aNTanaS", "aLgiRdas"];
-const myFriendsNames = friendsNames.map((v) => v.charAt(0).toUpperCase() + v.slice(1).toLowerCase());
+const myFriendsNames = friendsNames.map((name) => name[0].toUpperCase() + name.slice(1).toLowerCase());
 console.info(myFriendsNames);
 
 // 4 pratymas
 const myFriendsAge = [46, 35, 50, 44, 11, 13, 70];
-const oldFriendsAge = myFriendsAge.filter((v) => v >= 18);
+const oldFriendsAge = myFriendsAge.filter((age) => age >= 18);
 console.info(oldFriendsAge);
 
 // 5 pratymas
 const ltCities = ["Vilnius", "Kaunas", "Klaipėda", "Šiauliai", "Panevėžys"];
-const ltCitiesK = ltCities.find((v) => v.charAt(0) === "K");
+const ltCitiesK = ltCities.find((city) => city[0] === "K");
 console.info(ltCitiesK);
 
 // 6 pratymas
-const isLowerLaterFirst = ltCities.some((v) => v.charAt(0) === v.charAt(0).toLocaleLowerCase());
-console.info(isLowerLaterFirst);
+const isFirstLaterLower = ltCities.some((city) => city[0] === city[0].toLocaleLowerCase());
+console.info(isFirstLaterLower);
 
 // 7 pratymas
-const isFirstLetterAll = ltCities.every((v) => v.charAt(0) === v.charAt(0).toLocaleUpperCase());
-console.info(isFirstLetterAll);
+const isFirstLetterUpperAll = ltCities.every((city) => city[0] === city[0].toLocaleUpperCase());
+console.info(isFirstLetterUpperAll);
 
 
 

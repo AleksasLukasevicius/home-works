66fe539baf24fe790570518579524ca17ad34b19
ALEKSAS LUKAŠEVIČIUS <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2022-11-08 19:08:19 +0200

    gitignore

---
+++ NodeJS/node-post-express/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.post("/", (_, res) => {
+    res.send({ message: "Welcome to Alex project" });
+})
+
+app.listen(PORT, () => console.info(`Server is runnig on ${PORT} port`)) 

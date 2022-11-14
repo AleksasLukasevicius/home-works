94eafc81440226e72d368ab87ad95c106e0b08d1
AleksasLukasevicius <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2022-11-14 12:42:09 +0200

    CAO lesson 2

---
+++ NodeJS/CAO-Lesson2/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 5_000;
+
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Good")
+});
+
+app.listen(PORT, () => console.info(`Server is on ${PORT} port`)) 

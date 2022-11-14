8907f86a48af5a95d9fc9b126f690509850e6eda
ALEKSAS LUKAŠEVIČIUS <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2022-11-14 21:54:23 +0200

    CAO Lesson 2 NodeJS GET

--- NodeJS/CAO-Lesson2/cars-brands/script.js
+++ NodeJS/CAO-Lesson2/cars-brands/script.js
@@ -1,26 +1,25 @@
 const carList = async () => {
-    const cars = await getCars();  
-    const carsListElement = document.createElement("ul");
-  
-    cars.forEach((car) => {
-      const carElement = document.createElement("li");
-      carElement.textContent = car;
-      carsListElement.append(carElement);
-    });
-  
-    document.body.append(carsListElement);
-  };
-  
-  const getCars = async () => {
-    try {
-      const response = await fetch("http://localhost:5000");
-      const cars = await response.json();
-  
-      return cars;
-      
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  
-  carList();
+  const cars = await getCars();
+  const carsListElement = document.createElement("ul");
+
+  cars.forEach((car) => {
+    const carElement = document.createElement("li");
+    carElement.textContent = car;
+    carsListElement.append(carElement);
+  });
+
+  document.body.append(carsListElement);
+};
+
+const getCars = async () => {
+  try {
+    const response = await fetch("http://localhost:5000");
+    const cars = await response.json();
+
+    return cars;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+carList();

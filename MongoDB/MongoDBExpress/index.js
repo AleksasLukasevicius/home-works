a8fbd1ae97f9dce4059e397c25113ee99b4a0f3e
ALEKSAS LUKAŠEVIČIUS <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2022-11-17 00:25:28 +0200

    Mongo DB post

--- MongoDB/MongoDBExpress/index.js
+++ MongoDB/MongoDBExpress/index.js
@@ -11,14 +11,32 @@ const client = new MongoClient(URI);
 app.use(express.json());
 
 app.get("/", async (_, res) => {
-  const connection = await client.connect();
-  const data = await connection
-    .db("node-mongo-first-project")
-    .collection("users")
-    .find()
-    .toArray();
-  await connection.close();
-  return res.send(data);
+  try {
+    const connection = await client.connect();
+    const data = await connection
+      .db("node-mongo-first-project")
+      .collection("users")
+      .find()
+      .toArray();
+    await connection.close();
+    return res.send(data);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
+});
+
+app.post("/", async (_, res) => {
+  try {
+    const connection = await client.connect();
+    const dbRes = await connection
+      .db("node-mongo-first-project")
+      .collection("users")
+      .insertOne({ firstName: "Lukas", lastName: "Lukaševičius", age: 26 });
+    await connection.close();
+    return res.send(dbRes);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
 });
 
 app.post("/", (_, res) => {

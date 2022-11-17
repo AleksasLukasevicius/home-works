922d8a45401d828fd529302f5cd0309676cc3c24
ALEKSAS LUKAŠEVIČIUS <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2022-11-17 22:51:03 +0200

    Mongo DB post user

--- MongoDB/MongoDBExpress/index.js
+++ MongoDB/MongoDBExpress/index.js
@@ -1,5 +1,6 @@
 const { MongoClient } = require("mongodb");
 const express = require("express");
+const cors = require("cors");
 
 require("dotenv").config();
 
@@ -7,15 +8,18 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const URI = process.env.URI;
 const client = new MongoClient(URI);
+const DB = process.env.DB;
+const dbCollection = process.env.dbCollection;
 
 app.use(express.json());
+app.use(cors());
 
-app.get("/", async (_, res) => {
+app.get("/users", async (_, res) => {
   try {
     const connection = await client.connect();
     const data = await connection
-      .db("node-mongo-first-project")
-      .collection("users")
+      .db(DB)
+      .collection(dbCollection)
       .find()
       .toArray();
     await connection.close();
@@ -25,13 +29,27 @@ app.get("/", async (_, res) => {
   }
 });
 
-app.post("/", async (_, res) => {
+app.post("/user", async (req, res) => {
+  const { firstName, lastName } = req.body;
+
+  if (!firstName || !lastName) {
+    res.status(400).send("Nepateikti firstName ir lastName").end();
+    return;
+  }
+
+  if (typeof firstName !== "string" || typeof lastName !== "string") {
+    res.status(400).send(`${firstName} and ${lastName} are not a string`).end();
+    return;
+  }
+
   try {
     const connection = await client.connect();
-    const dbRes = await connection
-      .db("node-mongo-first-project")
-      .collection("users")
-      .insertOne({ firstName: "Lukas", lastName: "Lukaševičius", age: 26 });
+    const dbRes = await connection.db(DB).collection(dbCollection).insertOne({
+      firstName,
+      lastName,
+      isStudent: false,
+      age: 35,
+    });
     await connection.close();
     return res.send(dbRes);
   } catch (error) {

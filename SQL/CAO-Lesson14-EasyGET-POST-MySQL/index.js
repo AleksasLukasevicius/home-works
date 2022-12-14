6358185691373d7c8ad15fd3cba13f1673b739ea
ALEKSAS LUKAŠEVIČIUS <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2022-12-14 22:39:57 +0200

    CAO Lesson 14 SQL POST GET

--- SQL/CAO-Lesson14-EasyGET-POST-MySQL/index.js
+++ SQL/CAO-Lesson14-EasyGET-POST-MySQL/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mysql = require("mysql2/promise");
 
-require("dotenv").config;
+require("dotenv").config();
 
 const app = express();
 const SERVER_PORT = process.env.SERVER_PORT || 5_000;
@@ -11,7 +11,7 @@ const MYSQL_CONFIG = {
   host: process.env.host,
   port: process.env.port,
   database: process.env.database,
-  sslmode: process.env.sslmode,
+  // sslmode: process.env.sslmode,
 };
 
 console.info(MYSQL_CONFIG);
@@ -21,18 +21,104 @@ app.use(express.json());
 app.post("/table", async (req, res) => {
   const tableName = req.body?.tableName.trim();
 
+  if (!tableName) {
+    return res
+      .status(400)
+      .send(`Incorrect table name provided: ${tableName}`)
+      .end();
+  }
+
   try {
     const connection = await mysql.createConnection(MYSQL_CONFIG);
 
     await connection.execute(
-      `CREATE table ${tableName}(id int NOT NULL AUTO_INCREMENT, PRIMARY KEY (id), brand TEXT, model TEXT, size TEXT, price DECIMAL (10,2))`
+      `CREATE table ${tableName}(id int NOT NULL AUTO_INCREMENT, PRIMARY KEY (id), brand varchar(35), model varchar(35), size varchar(35), price DECIMAL (10,2))`
     );
-    await con.end();
-  } catch (err) {
-    res.status(500).send(err).end();
-    return console.error();
+
+    await connection.end();
+
+    res
+      .status(201)
+      .send({ message: `Table ${tableName} was created` })
+      .end();
+  } catch (error) {
+    res.status(500).send({ error }).end();
+    return console.error; //console.error({error})
   }
-  res.send("Table was created").end();
+});
+
+app.post("/shirt", async (req, res) => {
+  const brand = req.body?.brand.trim();
+  const model = req.body?.model.trim();
+  const size = req.body?.size.trim();
+  const { price } = req.body;
+
+  if (!brand || !model || !size || !price) {
+    return res.status(400).send(`not all shirt details are provided`).end();
+  }
+
+  try {
+    const connection = await mysql.createConnection(MYSQL_CONFIG);
+
+    await connection.execute(
+      `INSERT INTO shirts (brand, model, size, price) VALUES ('${brand}','${model}','${size}','${price}')`
+    );
+
+    await connection.end();
+
+    res
+      .status(201)
+      .send({
+        message: `Shirt Brand: ${brand}, Model: ${model}, Size: ${size} successfully created`,
+      })
+      .end();
+  } catch (error) {
+    res.status(500).send({ error }).end();
+    return console.error; //console.error({error})
+  }
+});
+
+app.get("/shirts", async (_, res) => {
+  try {
+    const connection = await mysql.createConnection(MYSQL_CONFIG);
+
+    const result = await connection.execute(
+      `SELECT * FROM shirts ORDER BY price LIMIT 10`
+    );
+
+    await connection.end();
+
+    res.status(201).send(result[0]).end();
+  } catch (error) {
+    res.status(500).send({ error }).end();
+    return console.error; //console.error({error})
+  }
+});
+
+app.get("/", async (_, res) => {
+  try {
+    const connection = await mysql.createConnection(MYSQL_CONFIG);
+
+    await connection.end();
+
+    res.status(201).send({ message: `Server is running` }).end();
+  } catch (error) {
+    res.status(500).send({ error }).end();
+    return console.error; //console.error({error})
+  }
+});
+
+app.get("*", async (_, res) => {
+  try {
+    const connection = await mysql.createConnection(MYSQL_CONFIG);
+
+    await connection.end();
+
+    res.status(404).send({ message: `Page not found` }).end();
+  } catch (error) {
+    res.status(500).send({ error }).end();
+    return console.error; //console.error({error})
+  }
 });
 
 app.listen(SERVER_PORT, () =>

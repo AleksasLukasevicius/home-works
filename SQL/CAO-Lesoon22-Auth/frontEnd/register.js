31ed89722d2d69eae80a4abd376a0fa9a4364d8c
ALEKSAS LUKAŠEVIČIUS <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2023-01-06 00:16:17 +0200

    CAO LEsson 22-23 front End

---
+++ SQL/CAO-Lesoon22-Auth/frontEnd/register.js
@@ -0,0 +1,46 @@
+const loginForm = document.querySelector("#login-form");
+
+loginForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const emailInputValue = document.querySelector("#email-input").value.trim();
+  const passwordInputValue = document
+    .querySelector("#password-input")
+    .value.trim();
+
+  const user = JSON.stringify({
+    email: emailInputValue,
+    password: passwordInputValue,
+  });
+
+  try {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-type", "application/json");
+
+    const response = await fetch(
+      "http://localhost:5000/v1/authorization/register",
+      {
+        method: "POST",
+        headers: myHeaders,
+        body: user,
+      }
+    );
+
+    if (response.ok) {
+      document.body.querySelector("#login-form").reset();
+      const data = await response.json();
+
+      localStorage.setItem("token", data.token);
+
+      window.location.assign(`./content.html`);
+    }
+
+    if (response.status >= 400) {
+      const message = await response.json();
+
+      alert(message.error);
+    }
+  } catch (error) {
+    alert(error.message);
+  }
+});

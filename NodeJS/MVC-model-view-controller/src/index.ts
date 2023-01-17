2a5f072333c91e85f51daa72913e6cadb3c742c6
AleksasLukasevicius <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2023-01-17 11:11:33 +0200

    controllers

--- NodeJS/MVC-model-view-controller/src/index.ts
+++ NodeJS/MVC-model-view-controller/src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { router } from "./controller/productController";
+import { router } from "./controllers/productController";
 
 const app = express();
 const PORT = 5_000;

fd886be3f276f36e94c3c3775a09128e4dd03481
ALEKSAS LUKAŠEVIČIUS <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2023-03-06 01:01:56 +0200

    mui card style

--- React/ecommerce-products-webapp/src/components/Products/Product.tsx
+++ React/ecommerce-products-webapp/src/components/Products/Product.tsx
@@ -1,8 +1,33 @@
 import { ProductsContext } from "..";
-import { type FC, useContext } from "react";
+import { type FC, useContext, useState } from "react";
 import type { TProductProps } from "./types";
 import { ProductActionButon } from "./ProductActionButton";
-import { Box } from "@mui/material";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+  Collapse,
+  IconButton,
+  IconButtonProps,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { styled } from "@mui/material/styles";
+interface ExpandMoreProps extends IconButtonProps {
+  expand: boolean;
+}
+
+const ExpandMore = styled((props: ExpandMoreProps) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 // export const Product = ({ product }: TProductProps) => {
 export const Product: FC<TProductProps> = ({ product }) => {
@@ -12,14 +37,39 @@ export const Product: FC<TProductProps> = ({ product }) => {
     //to do naudti objekta
     (cartProduct) => cartProduct.id === product.id
   );
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
   return (
-    <div className="product-card">
-      <h3>{product.title}</h3>
-      <p>{product.description}</p>
-      <p>Price: {product.price} €</p>
+    <Card sx={{ maxWidth: 255 }}>
+      <CardMedia
+        component="img"
+        alt={`${product.title}`}
+        // height="150"
+        // width="75"
+        src={`${product.image}`}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {product.title}
+        </Typography>
 
-      <Box>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography variant="body2" color="text.secondary">
+              {product.description}
+            </Typography>
+          </CardContent>
+        </Collapse>
+
+        <Typography variant="body2" color="text.secondary">
+          Price: {product.price} €
+        </Typography>
+      </CardContent>
+      <CardActions>
         <ProductActionButon
           title="+"
           type="addProduct"
@@ -32,7 +82,16 @@ export const Product: FC<TProductProps> = ({ product }) => {
             productId={product.id}
           />
         ) : null}
-      </Box>
-    </div>
+
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+    </Card>
   );
 };

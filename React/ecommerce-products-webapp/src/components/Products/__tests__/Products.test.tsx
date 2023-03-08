1b14c16b5fa49ea528b4ad6ba16f6a526cd95f54
ALEKSAS LUKAŠEVIČIUS <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2023-03-08 20:26:40 +0200

    test

--- React/ecommerce-products-webapp/src/components/Products/__tests__/Products.test.tsx
+++ React/ecommerce-products-webapp/src/components/Products/__tests__/Products.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import { Products } from "../Products";
 import renderer from "react-test-renderer";
-import { Header } from "../../Header";
 
 describe("Products", () => {
   it("should render Products correctly", () => {

6c6f2276c2bfa1f006184b560ea20c786003e498
ALEKSAS LUKAŠEVIČIUS <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2023-03-16 23:24:35 +0200

    storybook

--- React/ecommerce-products-webapp/src/components/Home/Home.tsx
+++ React/ecommerce-products-webapp/src/components/Home/Home.tsx
@@ -1,8 +1,54 @@
+import { FormEventHandler, useState } from "react";
+import { Button } from "@mui/material";
+
 export const Home = () => {
+  const [userData, setUserData] = useState({ email: "", password: "" });
+
+  const handleUserDataChange = (value: string, key: "email" | "password") => {
+    setUserData((prevUserData) => ({ ...prevUserData, [key]: value }));
+  };
+
+  const handleFormSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+
+    console.info(userData);
+  };
+
   return (
     <main>
       <h1>Welcome</h1>
       <h2>Home page</h2>
+
+      <form action="submit" method="post" onSubmit={handleFormSubmit}>
+        <fieldset>
+          <legend>User registration</legend>
+
+          <label htmlFor="email">e-mail</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="enter Your email"
+            value={userData.email}
+            onChange={(event) =>
+              handleUserDataChange(event.target.value, "email")
+            }
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="enter Your password"
+            value={userData.password}
+            onChange={(event) =>
+              handleUserDataChange(event.target.value, "password")
+            }
+          />
+
+          <Button type="submit" variant="outlined">
+            submit
+          </Button>
+        </fieldset>
+      </form>
     </main>
   );
 };
